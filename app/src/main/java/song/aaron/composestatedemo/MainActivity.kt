50ad18abc7e3929953474e8c8a551133e58028d5
Aaron Song <aaron@kmong.com>
2023-11-16 09:39:07 +0900

    fix: by 구문의 import

--- app/src/main/java/song/aaron/composestatedemo/MainActivity.kt
+++ app/src/main/java/song/aaron/composestatedemo/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import song.aaron.composestatedemo.ui.theme.ComposeStateDemoTheme
